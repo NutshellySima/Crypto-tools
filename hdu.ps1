bea7f41ab468ba97b3a7206a401ee65d1993042f
Chijun Sima <simachijun@qq.com>
2017-10-01 19:05:11 +0800

    Update hdu.ps1

--- hdu.ps1
+++ hdu.ps1
@@ -24,7 +24,7 @@
 # For more information, please refer to <http://unlicense.org/>
 
 function Change-HduHost() {
-    #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
     (Get-Content -Path "C:\Windows\System32\drivers\etc\hosts") -notmatch "218.75.123.182" | Out-File "C:\Windows\System32\drivers\etc\hosts"
     Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "`n218.75.123.182 bestcoder.hdu.edu.cn`n218.75.123.182 acm.hdu.edu.cn"
 }
