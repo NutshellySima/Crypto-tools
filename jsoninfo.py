9c39f6108e088115c047d4c0737d2a8bb6ce9c19
Chijun Sima <simachijun@qq.com>
2017-10-05 13:09:51 +0800

    Update jsoninfo.py

--- jsoninfo.py
+++ jsoninfo.py
@@ -18,6 +18,7 @@ import os
 import json
 import chardet
 
+count=0
 items=[x for x in os.listdir('.') if os.path.isfile(x)]
 
 for i in items:
@@ -26,6 +27,8 @@ for i in items:
     with open(i, 'r',encoding=encode.get('encoding')) as data:
         try:
             parsed=json.load(data)
+            count+=1
             print(parsed)
         except:
             print(i+" is skipped.")
+print(count)
